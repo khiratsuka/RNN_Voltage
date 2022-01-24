d28f6135a267874c76735b20159f859aec22dcc8
khiratsuka <4v4g1n.sakana.k4@gmail.com>
2022-01-25 01:09:19 +0900

    3版　Dataset構築済み、次回学習部分記述

--- path_test.py
+++ path_test.py
@@ -1,5 +1,40 @@
 import os
+import numpy as np
 
+"""
 test = sorted([s for s in os.listdir('./dataset/mage/train') if s.endswith('csv')])
 
+
+for t in test:
+    data_name_path = os.path.join('./dataset/mage/train', t)
+    if not os.path.join(data_name_path):
+        continue
+    print(data_name_path)
+myarray = np.loadtxt(data_name_path, delimiter='¥n')
+
+print(myarray)
+"""
+
+a = [1, 2, 3]
+b = [4, 5, 6]
+c = 777
+n = []
+
+num = 10 - len(a)
+for i in range(num):
+    n.append(c)
+test = [*n, *a]
 print(test)
+print(len(test))
+
+del test[-3:]
+print(test)
+print(len(test))
+
+"""
+test0 = [c, *a]
+test1 = [*a, *b]
+
+print(test0)
+print(test1)
+"""
--- rnn_volt.py
+++ rnn_volt.py
@@ -1,7 +1,8 @@
+import os
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import os
+import numpy as np
 from torch import optim
 from torch.utils.data import DataLoader
 from torch.utils.data import Dataset
@@ -11,7 +12,9 @@ import matplotlib.pyplot as plt
 
 #If GPU is available, use it
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-CLASS_NAMES = ['mage', 'nobashi']
+
+#setting classname
+CLASS_NAMES_DICT = {'mage':0, 'nobashi':1}
 
 class MyRNN(nn.Module):
     def __init__(self):
@@ -40,23 +43,56 @@ class EMGDataset(Dataset):
                  class_name='hoge',
                  is_train=True):
         assert class_name in CLASS_NAMES, 'class_name: {}, should be in {}'.format(class_name, CLASS_NAMES)
-        self.emg_data_paths = self._get_file_names(emg_data_folder)
+        self.emg_data_folder = emg_data_folder
         self.class_name = class_name
 
+        self.emg_data_path, self.correct_class = _get_file_names()
+
     def __getitem__(self, idx):
-        file_path = self.file_paths[idx]
+        emg_data_path, correct_class = self.emg_data_path[idx], self.correct_class[idx]
+        max_data_sampling_num = 24000
+        plus_data = 0.5
+
+        #loading data
+        emg_data = np.loadtxt(emg_data_path, delimiter='¥n')
+
+        #EMGのデータ長を揃える
+        #基準値に達していなかったら前padding, 超えていたら後ろのデータをカット
+        over_data_num = max_data_sampling_num - len(emg_data)
+        if over_data_num > 0:
+            plus_data_list = []
+            for i in range(plus_data_num):
+                plus_data_list.append(plus_data)
+            emg_data = [*plus_data, *emg_data]
+        elif over_data_num < 0:
+            del emg_data[over_data_num:]
+
+        return emg_data, correct_class
 
     def __len__(self):
-        #_get_file_namesでファイル数取得してreturnする
+        return len(self.emg_data_path)
 
     def _get_file_names(self):
         phase = 'train' if self.is_train else 'test'
-        emg_data, correct_class = [], []
+        emg_data_path, correct_class = [], []
 
+        #set directory path
         emg_data_dir = os.path.join(self.emg_data_folder, self.class_name, phase)
+        data_names = sorted([name for name in os.listdir(emg_data_dir) if name.endswith('csv')])
 
-        data_names = sorted([name for name in os.listdir('./dataset/mage/train') if name.endswith('csv')])
-        
+        #checking directory of csv data
+        for data_name in data_names:
+            data_name_path = os.path.join(emg_data_dir, data_name)
+            if not os.path.join(data_name_path):
+                continue
+            emg_data_path.extend(data_name_path)
+            correct_class.extend(CLASS_NAMES_DICT[self.class_name])
+
+        assert len(emg_data_path) == len(correct_class), 'number of emg_data and class are not same.'
+
+        return list(emg_data_path), list(correct_class)
+
+
 
 
 model = MyRNN().to(device)
